3a5b582bb7dc55a3211707eb20df8fff34018e74
Kamil Smolarek <smolarekkamil123@gmail.com>
2026-02-05 10:45:18 +0100

    project init

---
+++ src/main/java/com/kamilsmolarek/kafkaorders/KafkaordersApplication.java
@@ -0,0 +1,13 @@
+package com.kamilsmolarek.kafkaorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaordersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaordersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamilsmolarek/kafkaorders/KafkaordersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamilsmolarek.kafkaorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaordersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
